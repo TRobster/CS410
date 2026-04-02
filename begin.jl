70fbee22cf8a1c5c01d7516268c27ec4c8b7ea66
Trevor Robbins <trevorprobbins@gmail.com>
2026-04-01 23:00:07 -0700

    begin

--- begin.jl
+++ begin.jl
@@ -1,1 +1,13 @@
 println("hello world")
+
+
+function fibonacciSeq(x: int)
+    ### Returns a list of all numbers in the fibonacci Sequence up to number x
+    arr = Int[]
+    push(arr, 0)
+    for i in 1:x
+        
+    end
+end 
+α = π * 3.14
+println(α)
