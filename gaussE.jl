f78b98696176672ec1832560028e6fbdaab4361e
TRobster <trevorro@uoregon.edu>
2026-04-15 15:27:38 -0700

    Pushed

---
+++ gaussE.jl
@@ -0,0 +1,41 @@
+
+function I(n: Int64)
+    id = zeros(n, n)
+    for i in 1:n
+        id[i, i] = 1
+    end 
+    return id
+end 
+
+function forward(mat, n)
+    
+    L = zeros(n, n)
+
+    
+    for k in 1:n-1 
+        i_max = k 
+        v_max = abs(mat[i_max, k])
+        for i in k+1:n
+            if (abs(mat[i, k]) > v_max)
+                v_max = mat[i, k]
+                i_max = i
+            end
+        end
+
+
+        if i_max != k
+            mat[k], mat[i_max] = mat[i_max], mat[k]
+        end
+
+        for i in k+1:n
+            m = mat[i, k] / mat[k, k]
+            for j in k+1:n
+                mat[i, j] = mat[i, j] - mat[k, j] * m
+                end
+            mat[i, k] = 0
+        end
+    end     
+end 
+
+
+
