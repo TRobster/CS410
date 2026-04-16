ddcb56c9e140962ca1ea437803b166a34db47bb0
TRobster <trevorro@uoregon.edu>
2026-04-16 14:55:14 -0700

    Docstring integration

--- gaussE.jl
+++ gaussE.jl
@@ -1,6 +1,11 @@
 using Plots
 
 function eye(n)
+    #=
+    Basic identity matrix generation function, 
+        simply allocates, zeros L and U, then iterates
+        over the (i, i) diagonal for 1 placement
+    =#
     I = Matrix{Float64}(undef, n, n)
     I .= 0
     for i in 1:n
@@ -10,26 +15,38 @@ function eye(n)
 end
 
 function computeLUP(mat, n)
+    #=
+    Params: mat <-- an n x n symmetric positive definite (SPD) matrix
+            n <-- The base linear-dimension, when squared gives us our mat-dimension
+    Use: Given an n x n SPD-matrix, this function splits a matrix using gaussian elimination into
+        three different factors, that being its lower quadrant matrix (L), its upper quadrant matrix (U),
+        and the documentation of row swaps in the partial pivoting steps in a vector (P).
+    
+    =#
     
     L = eye(n)
+    # In theory P is an identity matrix, however for memory conservation its a vector with entries representing "1" on the (i, i)-diag
     P = collect(1:n)
 
+    # Main iteration for elimination, however first we must find the maximum absolute column value then swap for that row (partial pivoting)
     for k in 1:n-1 
         i_max = k 
         v_max = abs(mat[i_max, k])
         for i in k+1:n
+            # Partial pivoting
             if (abs(mat[i, k]) > v_max)
                 v_max = abs(mat[i, k])
                 i_max = i
             end
         end
 
-
+        # Records partial pivotting swap. 
         if i_max != k
             swap(mat, k, i_max, n)
             P[k], P[i_max] = P[i_max], P[k] 
         end
 
+        # Main elimination step. Computes numerically stable multiplier given the row below our current row. 
         for i in k+1:n
             m = mat[i, k] / mat[k, k]
             L[i, k] = m
@@ -44,6 +61,7 @@ end
 
 
 function swap(mat, i, j, n)
+    # Simple swapper help function 
     for k in 1:n
         temp = mat[i, k]
         mat[i, k] = mat[j, k]
@@ -52,9 +70,16 @@ function swap(mat, i, j, n)
 end 
 
 function forward(l, b, n)
+    #=  
+    Params: In basic format, our parameters correspond to the form Ly = Pb where
+        L = lower triangular portion of orignal matrix
+        b = Our original solution vector that integrates solving with use of immediate vector y
+        n = Same as before, dimension
+    =#
     y = zeros(n)
     for i in 1:n
         s = 0
+        # running sum 
         for j in 1:i-1
             s = s + l[i, j] * y[j]
         end
@@ -64,9 +89,17 @@ function forward(l, b, n)
 end 
 
 function backward(u, y, n)
+    #=
+    Params: In basic format, our parameters correspond to the form Ux = y
+        U = Upper triangular portion of original matrix
+        y = Solved porition-vector of L, now containing all solutions that can be substituted backwards down from n
+        n = Same as before, dimension
+    =#
     x = zeros(n)
+    # If y's solution began as the literal first entry, x's solution lies at the bottom-most entry, meaning a countdown loop is needed
     for i in n:-1:1
         s = 0 
+        # same logic as before, running sum
         for j in i+1:n
             s = s + u[i, j] * x[j]
         end 
@@ -77,6 +110,11 @@ end
     
 
 function LUPsolve(A, b, n)
+    #=
+    Params: A = Solvable SPD matrix with N x N entries 
+            b = Solution vector that must satisfy Ax = b where we solve for x 
+            n = Linear dimension of 
+    =#
     l, u, p = computeLUP(copy(A), n)
     y = forward(l, b[p], n)
     x = backward(u, y, n)
@@ -92,3 +130,4 @@ for N in [10, 100, 1000]
     println("N = $N")
     @time LUPsolve(A, b, N)
 end
+
